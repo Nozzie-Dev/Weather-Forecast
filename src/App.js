99e574dfd9ef40fd1901f3419eafb294f17f5435
Noziphezinhle Nzimande <125486198+Nozzie-Dev@users.noreply.github.com>
2024-05-09 19:51:29 +0200

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import WeatherForecast from "./weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>and save to reload.</p>
         <WeatherForecast />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
