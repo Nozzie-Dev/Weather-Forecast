9aa2603b90c78ac0a0610ad6c8e997358569b991
Noziphezinhle Nzimande <125486198+Nozzie-Dev@users.noreply.github.com>
2024-05-09 20:21:50 +0200

    Update weather.js

--- src/weather.js
+++ src/weather.js
@@ -21,7 +21,17 @@ function WeatherForecast() {
         axios
           .get(`${api.base}forecast?q=${query}&units=metric&APPID=${api.key}`)
           .then((res) => {
-            setForecast(res.data.list.filter((item, index) => index % 8 === 0));
+            // Filter forecast for the next 6 days
+            const filteredForecast = res.data.list
+              .filter((item) => {
+                const currentDate = new Date().getDate();
+                const forecastDate = new Date(item.dt * 1000).getDate();
+                return (
+                  forecastDate !== currentDate && forecastDate > currentDate
+                );
+              })
+              .slice(0, 6);
+            setForecast(filteredForecast);
           });
       })
       .catch((err) => {
@@ -87,65 +97,65 @@ function WeatherForecast() {
         </div>
       </div>
       {typeof weather.main !== "undefined" ? (
-        <div>
-          <div className="row">
-            <div className="col-md-6">
-              <div className="card">
-                <div className="card-header">
-                  <h5>
-                    {weather.name}, {weather.sys.country}
-                  </h5>
-                  <p>{dateBuilder(new Date())}</p>
-                </div>
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-md-6">
-                      <h6>{Math.round(weather.main.temp)}°C</h6>
-                      <img
-                        src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
-                        alt="weather-icon"
-                      />
-                      <p>{weather.weather[0].description}</p>
-                    </div>
-                    <div className="col-md-6">
-                      <p>Humidity: {weather.main.humidity}%</p>
-                      <p>Wind: {weather.wind.speed} km/h</p>
-                    </div>
-                  </div>
-                </div>
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-header">
+                <h5>
+                  {weather.name}, {weather.sys.country}
+                </h5>
+                <p>{dateBuilder(new Date())}</p>
               </div>
-            </div>
-          </div>
-          <div className="row mt-3">
-            <div className="col-md-12">
-              <h4>Six Day Forecast</h4>
-              <div className="row">
-                {forecast.map((item, index) => (
-                  <div className="col-md-2" key={index}>
-                    <div className="card">
-                      <div className="card-body">
-                        <h6>{dateBuilder(new Date(item.dt * 1000))}</h6>
-                        <p>Max: {Math.round(item.main.temp_max)}°C</p>
-                        <p>Min: {Math.round(item.main.temp_min)}°C</p>
-                        <img
-                          src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
-                          alt="weather-icon"
-                        />
-                      </div>
-                    </div>
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-md-6">
+                    <h6>{Math.round(weather.main.temp)}°C</h6>
+                    <img
+                      src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+                      alt="weather-icon"
+                    />
+                    <p>{weather.weather[0].description}</p>
                   </div>
-                ))}
+                  <div className="col-md-6">
+                    <p>Humidity: {weather.main.humidity}%</p>
+                    <p>Wind: {weather.wind.speed} km/h</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       ) : (
-        <div className="row">
+        <div className="row justify-content-center">
           <div className="col-md-6">
             <div className="card">
               <div className="card-body text-center">
                 <h3>Enter a city to find the weather forecast for the week</h3>
               </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {forecast.length > 0 && (
+        <div className="row mt-3 justify-content-center">
+          <div className="col-md-6">
+            <h4>Six Day Forecast</h4>
+            <div className="row">
+              {forecast.map((item, index) => (
+                <div className="col-md-2" key={index}>
+                  <div className="card">
+                    <div className="card-body">
+                      <h6>{dateBuilder(new Date(item.dt * 1000))}</h6>
+                      <p>Max: {Math.round(item.main.temp_max)}°C</p>
+                      <p>Min: {Math.round(item.main.temp_min)}°C</p>
+                      <img
+                        src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+                        alt="weather-icon"
+                      />
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
