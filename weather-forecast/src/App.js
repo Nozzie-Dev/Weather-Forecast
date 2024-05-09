e2a90c33b7abb8ab9447836b2b5f3854e0b5c5a5
Noziphezinhle Nzimande <125486198+Nozzie-Dev@users.noreply.github.com>
2024-05-09 19:24:11 +0200

    Update App.js

--- weather-forecast/src/App.js
+++ weather-forecast/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>and save to reload.</p>
+        <p>React Weather App</p>
         <a
           className="App-link"
           href="https://reactjs.org"
